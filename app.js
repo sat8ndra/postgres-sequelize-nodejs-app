8cb73f9a621156da959fd5cb04203247454ffd90
Satyendra Gupta <sat8ndra@gmail.com>
2020-01-25 01:59:53 +0530

    Added views template

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const app = express();
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => res.send("INDEX"));
 
 app.use("/gigs", require("./routes/gigs"));
