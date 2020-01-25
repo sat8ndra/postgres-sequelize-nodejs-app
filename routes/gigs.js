0e18bf93d140dea25e2c4f8800517424d3b3639b
Satyendra Gupta <sat8ndra@gmail.com>
2020-01-26 01:56:44 +0530

    Added error checking in add form fields

--- routes/gigs.js
+++ routes/gigs.js
@@ -19,18 +19,34 @@ router.get("/add", (req, res) => res.render("add"));
 
 // Add a gig
 router.post("/add", (req, res) => {
-  const data = {
-    title: "Looking for a Angular developer",
-    technologies: "react,javascript,html,css",
-    budget: "$3000",
-    decsription:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et dui tortor. Praesent sit amet neque odio. Vestibulum eu nibh libero. Aenean diam velit, accumsan id enim et, vulputate vulputate ante. Aliquam condimentum risus vel libero condimentum vehicula. Morbi imperdiet massa a tempus pulvinar. Praesent tempus sem sed felis bibendum pretium. Suspendisse nulla risus, vehicula vel dui nec, fermentum porttitor ex. Quisque consequat nec dui sed accumsan. ",
-    contact_email: "user1@gmail.com"
-  };
+  let { title, technologies, budget, description, contact_email } = req.body;
+  let errors = [];
 
-  let { title, technologies, budget, description, contact_email } = data;
+  // Validate fields
+  if (!title) {
+    errors.push({ text: "Please add a title" });
+  }
+  if (!technologies) {
+    errors.push({ text: "Please add some technologies" });
+  }
+  if (!description) {
+    errors.push({ text: "Please add a description" });
+  }
+  if (!contact_email) {
+    errors.push({ text: "Please add a contact email" });
+  }
 
-  //Insert into table
+  if(errors.length > 0) {
+    res.render("add", {
+      errors,
+      title,
+      technologies,
+      description,
+      budget,
+      contact_email
+    });
+  } else {
+    //Insert into table
   Gig.create({
     title,
     technologies,
@@ -41,5 +57,8 @@ router.post("/add", (req, res) => {
     .then(gig => res.redirect("/gigs"))
     .catch(err => console.log(err));
 });
+  }
+
+  
 
 module.exports = router;
